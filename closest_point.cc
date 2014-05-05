188c09c6adff056e4882862d7a3992679cc3f673
rohit1729 <rohitranjan1729@gmail.com>
2014-05-05 15:49:35 +0530

    Update closest_point.cc

--- closest_point.cc
+++ closest_point.cc
@@ -1,109 +1,363 @@
+/*
+ * C++ Program to Implement shortest pair 2D
+ */
 #include <iostream>
-#include <vector>
-#include <utility>
-#include <cmath>
-#include <random>
-#include <chrono>
+#include <cstdlib>
+#include <omp.h>
+#include <stdio.h>
 #include <algorithm>
-#include <iterator>
- 
-typedef std::pair<double, double> point_t;
-typedef std::pair<point_t, point_t> points_t;
- 
-double distance_between(const point_t& a, const point_t& b) {
-	return std::sqrt(std::pow(b.first - a.first, 2)
-		+ std::pow(b.second - a.second, 2));
-}
- 
-std::pair<double, points_t> find_closest_brute(const std::vector<point_t>& points) {
-	if (points.size() < 2) {
-		return { -1, { { 0, 0 }, { 0, 0 } } };
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <time.h>
+using namespace std;
+//#include <conio>
+struct Point{
+	float x,y;
+};
+void merge(Point *,int, int , int, Point*);
+void mergey(Point *,int, int , int, Point*);
+double closest_pair_dnq(Point *b,Point *bc,Point* bcy,int low,int high,int arrayln){
+	int mid=floor((low+high)/2);
+	double totalmin;
+	double minl;
+	double minr;
+//cout<<"low: "<<low<<"high: "<<high<<"medium"<<mid<<"\n";
+	
+if(high-low<=20){
+		double dist= INT_MAX;;
+		for(int j=low;j<=high;j++){
+			for(int k=(j+1) ;k<=high;k++){
+				if(j !=k){
+				double d=sqrt((b[j].y-b[k].y)*(b[j].y-b[k].y)+(b[j].x-b[k].x)*(b[j].x-b[k].x));
+				//cout<<"the distance from d complex"<<d<<"the j val"<<j<<"the k val"<<k<<"distance"<<d<<"\n";
+				if(d<dist){
+					dist=d;
+					
+				}
+				}
+				
+			}
+		}
+		return dist;
 	}
-	auto minDistance = std::abs(distance_between(points.at(0), points.at(1)));
-	points_t minPoints = { points.at(0), points.at(1) };
-	for (auto i = std::begin(points); i != (std::end(points) - 1); ++i) {
-		for (auto j = i + 1; j < std::end(points); ++j) {
-			auto newDistance = std::abs(distance_between(*i, *j));
-			if (newDistance < minDistance) {
-				minDistance = newDistance;
-				minPoints.first = *i;
-				minPoints.second = *j;
+	else{
+		
+		minl=closest_pair_dnq(b,bc,bcy,low,mid,arrayln);	
+
+
+        minr=closest_pair_dnq(b,bc,bcy,mid,high,arrayln);
+
+
+	totalmin=min(minl,minr);
+	vector<Point> finalarr;
+	for(int m=0;m<arrayln;m++){
+		if(bcy[m].x >= bc[mid].x-totalmin && bcy[m].x <= bc[mid].x+totalmin){
+			finalarr.push_back(bcy[m]);
+			}
+	}
+for( int i = 0; i < finalarr.size(); i++ ) {
+	if(finalarr[i].x <=bc[mid].x){
+	for(int j= i-8; j<=i+8; j++ ) {
+		if((i!=j) && (j>=0) && (j<finalarr.size())){
+			double d=sqrt((finalarr[i].y-finalarr[j].y)*(finalarr[i].y-finalarr[j].y)+(finalarr[i].x-finalarr[j].x)*(finalarr[i].x-finalarr[j].x));
+			if(d < totalmin){
+				totalmin=d;
 			}
 		}
 	}
-	return { minDistance, minPoints };
 }
- 
-std::pair<double, points_t> find_closest_optimized(const std::vector<point_t>& xP,
-	const std::vector<point_t>& yP) {
-	if (xP.size() <= 3) {
-		return find_closest_brute(xP);
+}
+return totalmin;
+
+		
+		
+}
+	
+}
+double closest_pair_dnqp(Point *b,Point *bc,Point* bcy,int low,int high,int arrayln){
+	int tid;
+	int mid=floor((low+high)/2);
+	double totalmin;
+	double minl;
+	double minr;
+//cout<<"low: "<<low<<"high: "<<high<<"medium"<<mid<<"\n";
+	
+if(high-low<=40){
+		double dist= INT_MAX;;
+		for(int j=low;j<=high;j++){
+			for(int k=(j+1) ;k<=high;k++){
+				double d=sqrt((b[j].y-b[k].y)*(b[j].y-b[k].y)+(b[j].x-b[k].x)*(b[j].x-b[k].x));
+				//cout<<"the distance from d complex"<<d<<"the j val"<<j<<"the k val"<<k<<"distance"<<d<<"\n";
+				if(d<dist){
+					dist=d;
+					
+				}
+				
+			}
+		}
+		return dist;
 	}
-	auto N = xP.size();
-	auto xL = std::vector<point_t>();
-	auto xR = std::vector<point_t>();
-	std::copy(std::begin(xP), std::begin(xP) + (N / 2), std::back_inserter(xL));
-	std::copy(std::begin(xP) + (N / 2), std::end(xP), std::back_inserter(xR));
-	auto xM = xP.at(N / 2).first;
-	auto yL = std::vector<point_t>();
-	auto yR = std::vector<point_t>();
-	std::copy_if(std::begin(yP), std::end(yP), std::back_inserter(yL), [&xM](const point_t& p) {
-		return p.first <= xM;
-	});
-	std::copy_if(std::begin(yP), std::end(yP), std::back_inserter(yR), [&xM](const point_t& p) {
-		return p.first > xM;
-	});
-	auto p1 = find_closest_optimized(xL, yL);
-	auto p2 = find_closest_optimized(xR, yR);
-	auto minPair = (p1.first <= p2.first) ? p1 : p2;
-	auto yS = std::vector<point_t>();
-	std::copy_if(std::begin(yP), std::end(yP), std::back_inserter(yS), [&minPair, &xM](const point_t& p) {
-		return std::abs(xM - p.first) < minPair.first;
-	});
-	auto result = minPair;
-	for (auto i = std::begin(yS); i != (std::end(yS) - 1); ++i) {
-		for (auto k = i + 1; k != std::end(yS) &&
-		 ((k->second - i->second) < minPair.first); ++k) {
-			auto newDistance = std::abs(distance_between(*k, *i));
-			if (newDistance < result.first) {
-				result = { newDistance, { *k, *i } };
+	else{
+#pragma omp task untied shared(b,bc,bcy,low,mid,arrayln,minl)
+{
+	//int tid= omp_get_thread_num();
+	//cout<<"the thread id"<<tid<<"\n";
+		minl=closest_pair_dnqp(b,bc,bcy,low,mid,arrayln);
+	}
+#pragma omp task untied shared(b,bc,bcy,low,mid,arrayln,minr)
+{
+		minr=closest_pair_dnqp(b,bc,bcy,mid,high,arrayln);
+	}
+		#pragma omp taskwait
+{
+	
+		totalmin=min(minl,minr);
+		vector<Point> finalarr;
+		for(int m=0;m<arrayln;m++){
+			if(bcy[m].x >= bc[mid].x-totalmin && bcy[m].x <= bc[mid].x+totalmin){
+				finalarr.push_back(bcy[m]);
+				}
+		}
+	for( int i = 0; i < finalarr.size(); i++ ) {
+		if(finalarr[i].x <=bc[mid].x){
+		for(int j= i-8; j<=i+8; j++ ) {
+			if((i!=j) && (j>=0) && (j<finalarr.size())){
+				double d=sqrt((finalarr[i].y-finalarr[j].y)*(finalarr[i].y-finalarr[j].y)+(finalarr[i].x-finalarr[j].x)*(finalarr[i].x-finalarr[j].x));
+				if(d < totalmin){
+					totalmin=d;
+				}
 			}
 		}
 	}
-	return result;
+	}
+	return totalmin;
 }
- 
-void print_point(const point_t& point) {
-	std::cout << "(" << point.first
-		<< ", " << point.second
-		<< ")";
 }
- 
-int main(int argc, char * argv[]) {
-	std::default_random_engine re(std::chrono::system_clock::to_time_t(
-		std::chrono::system_clock::now()));
-	std::uniform_real_distribution<double> urd(-500.0, 500.0);
-	std::vector<point_t> points(100);
-	std::generate(std::begin(points), std::end(points), [&urd, &re]() {
-                return point_t { 1000 + urd(re), 1000 + urd(re) };
-        });
-	auto answer = find_closest_brute(points);
-	std::sort(std::begin(points), std::end(points), [](const point_t& a, const point_t& b) {
-		return a.first < b.first;
-	});
-	auto xP = points;
-	std::sort(std::begin(points), std::end(points), [](const point_t& a, const point_t& b) {
-		return a.second < b.second;
-	});
-	auto yP = points;
-	std::cout << "Min distance (brute): " << answer.first << " ";
-	print_point(answer.second.first);
-	std::cout << ", ";
-	print_point(answer.second.second);
-	answer = find_closest_optimized(xP, yP);
-	std::cout << "\nMin distance (optimized): " << answer.first << " ";
-	print_point(answer.second.first);
-	std::cout << ", ";
-	print_point(answer.second.second);
-	return 0;
+
+	
+}
+int fib(int n)
+{
+  int i, j;
+  if (n<2)
+    return n;
+  else
+    {
+       #pragma omp task untied shared(i) firstprivate(n)
+      {
+      	i=fib(n-1);
+		int k=omp_get_thread_num();
+		//printf("Thread %d doing task 1\n",k);
+		//cout<<"the thread num is "<<k<<"\n";
+      } 
+	   
+
+       #pragma omp task untied shared(j) firstprivate(n)
+       j=fib(n-2);
+
+       #pragma omp taskwait
+       return i+j;
+    }
+}
+void mergesorty(Point *a, int low, int high, Point* sc)
+{
+    int mid;
+    if (low < high)
+    {
+        mid=(low+high)/2;
+        mergesorty(a,low,mid,sc);
+        mergesorty(a,mid+1,high,sc);
+        mergey(a,low,high,mid,sc);
+    }
+    return;
+}
+void mergey(Point *a, int low, int high, int mid,Point* sc)
+{
+    int i, j, k;
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high)
+    {
+        if (a[i].y < a[j].y)
+        {
+            sc[k].y = a[i].y;
+			sc[k].x=a[i].x;
+            k++;
+            i++;
+        }
+        else
+        {
+            sc[k].y = a[j].y;
+			sc[k].x=a[j].x;
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid)
+    {
+        sc[k].y = a[i].y;
+		sc[k].x = a[i].x;
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        sc[k].y = a[j].y;
+		sc[k].x = a[j].x;
+        k++;
+        j++;
+    }
+    for (i = low; i < k; i++)
+    {
+        a[i].y = sc[i].y;
+		a[i].x = sc[i].x;
+    }
+}
+void mergesort(Point *a, int low, int high, Point* sc)
+{
+    int mid;
+    if (low < high)
+    {
+        mid=(low+high)/2;
+        mergesort(a,low,mid,sc);
+        mergesort(a,mid+1,high,sc);
+        merge(a,low,high,mid,sc);
+    }
+    return;
+}
+void merge(Point *a, int low, int high,int mid,Point *sc)
+{
+    int i, j, k;
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high)
+    {
+        if (a[i].x < a[j].x)
+        {
+            sc[k].x = a[i].x;
+			sc[k].y = a[i].y;
+            k++;
+            i++;
+        }
+        else
+        {
+            sc[k].x = a[j].x;
+			sc[k].y = a[j].y;
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid)
+    {
+        sc[k].x = a[i].x;
+		sc[k].y = a[i].y;
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        sc[k].x = a[j].x;
+		sc[k].y = a[j].y;
+        k++;
+        j++;
+    }
+    for (i = low; i < k; i++)
+    {
+        a[i].y = sc[i].y;
+		a[i].x = sc[i].x;
+    }
+
+}
+int main()
+{
+	 int nfib = 10;
+    int i,num;
+    cout<<"enter number of the points\n";
+	cin>>num;
+	Point *a;
+	Point *ac;
+	Point *acy;
+	ac=new Point[num];
+	a=new Point[num];
+	acy=new Point[num];
+	
+    for (i = 0; i < num; i++)
+    {
+        //a[i]=(rand() % 1024)+1;
+		acy[i].x=ac[i].x=a[i].x= static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/1024.12));
+		acy[i].y=ac[i].y=a[i].y= static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/906.12));
+		//cout<<a[i].x<<"and"<<a[i].y<<"the i valie is"<<i<<"\n";
+    }
+	//cout<<"allocation has been done";
+
+    //mergesort(a, 0, (num-1));
+	clock_t start, end;
+	     double cpu_time_used;
+		 Point *sc;
+		sc=new Point[num];
+	   start = clock();
+	    
+	double dist= INT_MAX;
+	for(int j=0;j<num;j++){
+		for(int k=(j+1);k<num;k++){
+			
+					double d=sqrt((a[j].y-a[k].y)*(a[j].y-a[k].y)+(a[j].x-a[k].x)*(a[j].x-a[k].x));
+					
+				
+					//cout<<"the dis simple is x"<<d<<"j value is"<<j<<"k value is"<<k<<"\n";
+			if(d<dist){
+				dist=d;
+			}
+			
+		}
+	}
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	
+	cout<<"Closest pair normal method"<<dist<<"time taken by this method"<<cpu_time_used<<"\n";
+	
+	sc=new Point[num];
+start = clock();
+	mergesort(ac, 0, (num-1),sc);
+    for (i = 0; i < num; i++)
+    {
+		//cout<<ac[i].x<< "and" <<ac[i].y<<"the i value is"<<i<<"\n";
+    }
+	
+	sc=new Point[num];
+mergesorty(acy,0,(num-1),sc);
+    for (i = 0; i < num; i++)
+    {
+        //cout<<acy[i].x<< "and y arranged" <<acy[i].y<<"\n";
+    }
+	
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	cout<<"Time taken to sort"<<cpu_time_used<<"\n";
+	start = clock();
+	double dis=closest_pair_dnq(a,ac,acy,0,num-1,num);
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	cout<<"Min distance is divide and conquer method"<< dis<<"time taken by this func"<<cpu_time_used<<"\n";
+    omp_set_dynamic(0);
+    omp_set_num_threads(4);
+	
+#pragma omp parallel shared(a,ac,acy,num)
+	{
+#pragma omp single
+		{
+			double start=omp_get_wtime();
+			double dis=closest_pair_dnqp(a,ac,acy,0,num-1,num);
+			double end=omp_get_wtime();
+			double prtime=(double(end-start));
+			cout<<"the procressing time for parallel distance"<<dis<<"time"<<prtime<<"\n";
+			
+			 //printf ("fib(%d) = %d\n", nfib, fib(nfib));
+	}
+
+}
+	
 }
